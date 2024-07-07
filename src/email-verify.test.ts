666c8a4a05f4973e0127cc1d388974bf92a26cea
Shigoto-dev19 <shigoto-dev19@protonmail.com>
2024-07-07 12:19:17 +0100

    Refactor emailVerify tests

--- src/email-verify.test.ts
+++ src/email-verify.test.ts
@@ -4,7 +4,44 @@ import { Bigint2048 } from 'o1js-rsa';
 import { emailVerify } from './email-verify.js';
 import { EmailVerifyInputs, generateInputs } from './generate-inputs.js';
 
-describe('emailVerify', () => {
+/**
+ * Tests the email verification process using the provided inputs.
+ *
+ * @param inputs - The inputs required for the email verification.
+ * @param ignoreBodyHashCheck - Flag to ignore the body hash check: default=true.
+ * @param errorMessage - The expected error message if an error is expected.
+ */
+function testEmailVerify(
+  inputs: EmailVerifyInputs,
+  ignoreBodyHashCheck = true,
+  errorMessage?: string
+) {
+  // Function to call the emailVerify function with the provided inputs
+  const callEmailVerify = () =>
+    emailVerify(
+      inputs.paddedHeader,
+      inputs.headerHashIndex,
+      inputs.signature,
+      inputs.publicKey,
+      inputs.modulusLength,
+      !ignoreBodyHashCheck,
+      inputs.paddedBodyRemainingBytes,
+      inputs.precomputedHash,
+      inputs.bodyHashIndex,
+      inputs.headerBodyHashIndex
+    );
+
+  // If an error message is provided, expect the emailVerify function to throw an error with the specified message
+  if (errorMessage) {
+    expect(callEmailVerify).toThrow(errorMessage);
+  }
+  // If no error message is provided, expect the emailVerify function to execute without throwing an error
+  else {
+    expect(callEmailVerify).not.toThrow();
+  }
+}
+
+describe('emailVerify: email-good', () => {
   let inputs: EmailVerifyInputs;
 
   beforeAll(async () => {
@@ -12,91 +49,47 @@ describe('emailVerify', () => {
     inputs = await generateInputs(rawEmail);
   });
 
-  it('should verify test email with no bodyHashCheck - correct body', async () => {
-    // Call the provable emailVerify function
-    emailVerify(
-      inputs.paddedHeader,
-      inputs.headerHashIndex,
-      inputs.signature,
-      inputs.publicKey,
-      inputs.modulusLength,
-      false,
-      inputs.paddedBodyRemainingBytes,
-      inputs.precomputedHash,
-      inputs.bodyHashIndex,
-      inputs.headerBodyHashIndex
-    );
+  it('should verify test email with bodyHashCheck disabled - correct body', async () => {
+    testEmailVerify(inputs);
   });
 
-  it('should verify test email with no bodyHashCheck - incorrect body', async () => {
-    emailVerify(
-      inputs.paddedHeader,
-      inputs.headerHashIndex,
-      inputs.signature,
-      inputs.publicKey,
-      inputs.modulusLength,
-      false,
-      Bytes(128).random(),
-      inputs.precomputedHash,
-      inputs.bodyHashIndex,
-      inputs.headerBodyHashIndex
-    );
+  it('should verify test email with bodyHashCheck disabled - incorrect body', async () => {
+    const tamperedBodyBytes = Bytes(128).random();
+    const tamperedInputs = {
+      ...inputs,
+      paddedBodyRemainingBytes: tamperedBodyBytes,
+    };
+
+    testEmailVerify(tamperedInputs, true);
   });
 
-  it('should verify test email with bodyHashCheck', async () => {
-    emailVerify(
-      inputs.paddedHeader,
-      inputs.headerHashIndex,
-      inputs.signature,
-      inputs.publicKey,
-      inputs.modulusLength,
-      true,
-      inputs.paddedBodyRemainingBytes,
-      inputs.precomputedHash,
-      inputs.bodyHashIndex,
-      inputs.headerBodyHashIndex
-    );
+  it('should verify test email with bodyHashCheck enabled', async () => {
+    testEmailVerify(inputs, false);
   });
 
+  //TODO Update RSA verification to throw meaningful error messages
   it('should fail if the DKIM signature is wrong', async () => {
     // Use a random invalid DKIM signature
     const invalidSignature = Bigint2048.from(1234567n);
-    expect(() => {
-      emailVerify(
-        inputs.paddedHeader,
-        inputs.headerHashIndex,
-        invalidSignature,
-        inputs.publicKey,
-        inputs.modulusLength,
-        false,
-        inputs.paddedBodyRemainingBytes,
-        inputs.precomputedHash,
-        inputs.bodyHashIndex,
-        inputs.headerBodyHashIndex
-      );
-    }).toThrow();
+    const tamperedInputs = { ...inputs, signature: invalidSignature };
+
+    const errorMessage =
+      'Field.assertEquals(): 49157264413748767276814317779506976 != 42872445006125824354192737719310854';
+    testEmailVerify(tamperedInputs, true, errorMessage);
+    testEmailVerify(tamperedInputs, false, errorMessage);
   });
 
   it('should fail if DKIM message (headers) is tampered with', async () => {
-    // Tamper with the headers bytes
-    const tamperedHeadersBytes = Bytes.from([
+    const tamperedHeaderBytes = Bytes.from([
       ...Bytes(64).random().bytes,
       ...inputs.paddedHeader.bytes,
     ]);
-    expect(() => {
-      emailVerify(
-        tamperedHeadersBytes,
-        inputs.headerHashIndex,
-        inputs.signature,
-        inputs.publicKey,
-        inputs.modulusLength,
-        false,
-        inputs.paddedBodyRemainingBytes,
-        inputs.precomputedHash,
-        inputs.bodyHashIndex,
-        inputs.headerBodyHashIndex
-      );
-    }).toThrow();
+    const tamperedInputs = { ...inputs, paddedHeader: tamperedHeaderBytes };
+
+    // Error message stemming from dynamic SHA256 padding
+    const errorMessage = 'Padding error at index 161: expected zero.';
+    testEmailVerify(tamperedInputs, true, errorMessage);
+    testEmailVerify(tamperedInputs, false, errorMessage);
   });
 
   it('should fail if the email body is tampered with', async () => {
@@ -105,38 +98,24 @@ describe('emailVerify', () => {
       ...inputs.paddedBodyRemainingBytes.bytes,
       UInt8.from(1),
     ]);
-    expect(() => {
-      emailVerify(
-        inputs.paddedHeader,
-        inputs.headerHashIndex,
-        inputs.signature,
-        inputs.publicKey,
-        inputs.modulusLength,
-        true, // Enable body hash check since we are tampering with the body
-        tamperedBodyBytes,
-        inputs.precomputedHash,
-        inputs.bodyHashIndex,
-        inputs.headerBodyHashIndex
-      );
-    }).toThrow();
+    const tamperedInputs = {
+      ...inputs,
+      paddedBodyRemainingBytes: tamperedBodyBytes,
+    };
+
+    // Error message stemming from dynamic SHA256 padding
+    const errorMessage = 'Array length must be a multiple of 16';
+    testEmailVerify(tamperedInputs, false, errorMessage);
   });
 
-  it('should fail if the email bodyHashIndex is false', async function () {
+  //TODO Update bodyHash assertion to throw a meaningful error message
+  it('should fail if the email bodyHashIndex is false', async () => {
     // Tamper with the body hash
-    const falseBodyHashIndex = inputs.bodyHashIndex.add(Field.random());
-    expect(() => {
-      emailVerify(
-        inputs.paddedHeader,
-        inputs.headerHashIndex,
-        inputs.signature,
-        inputs.publicKey,
-        inputs.modulusLength,
-        true, // Enable body hash check since we are tampering with the body hash
-        inputs.paddedBodyRemainingBytes,
-        inputs.precomputedHash,
-        falseBodyHashIndex,
-        inputs.headerBodyHashIndex
-      );
-    }).toThrow();
+    const falseBodyHashIndex = Field(33);
+    const tamperedInputs = { ...inputs, bodyHashIndex: falseBodyHashIndex };
+
+    // Error message stemming from non-compliant bodyHash assertion
+    const errorMessage = 'Field.assertEquals(): 56 != 97';
+    testEmailVerify(tamperedInputs, false, errorMessage);
   });
 });
